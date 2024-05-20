7ef7e7cf880f8e487fe0d9bf02fac2ea3e73724e
sleepyren <96728203+sleepyren@users.noreply.github.com>
2024-05-20 11:40:32 -0400

    added CORS annotation for the frontend

--- src/main/java/com/renaldo/libraryservice/LibraryCatalogResource.java
+++ src/main/java/com/renaldo/libraryservice/LibraryCatalogResource.java
@@ -2,6 +2,7 @@ package com.renaldo.libraryservice;
 
 import com.renaldo.libraryservice.models.LibraryItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,8 @@ public class LibraryCatalogResource {
         return "Saved";
     }
 
+    //Enables cross origin requests from the frontend. The server will approve a preflight CORS request
+    @CrossOrigin(origins = "http://localhost:5173")
     @RequestMapping("/findall")
     public List<LibraryItem> findAll(){
         return repository.findAll();
